018d944bdb2f5677aa6bec0d98ad969193c41d20
yihaocompany <574574@qq.com>
2018-04-13 19:37:29 +0800

    '加入SQL安装目录'

---
+++ app/modules/install/controllers/ControllerBase.php
@@ -0,0 +1,9 @@
+<?php
+namespace Shenxianshop\Modules\Install\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+class ControllerBase extends Controller
+{
+
+}
---
+++ app/modules/install/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Shenxianshop\Modules\Install\Controllers;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
